97f413ecd8fec02693a8338f2a7de625172132ac
sjlee80 <sjlee80@gmail.com>
2025-02-01 13:44:25 +0900

    fix: boilerplate

--- backend/data-server/src/app.module.ts
+++ backend/data-server/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import mongodbConfig from './common/config/mongodb.config';
+import swaggerConfig from './common/config/swagger.config';
+import aiAgentConfig from './common/config/ai-agent.config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env`,
+      load: [mongodbConfig, swaggerConfig, aiAgentConfig],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('mongodb.url'),
+        user: configService.get<string>('mongodb.user'),
+        pass: configService.get<string>('mongodb.pass'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/data-server/src/main.ts
+++ backend/data-server/src/main.ts
@@ -1,8 +1,52 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as basicAuth from 'express-basic-auth';
+import { ConfigService } from '@nestjs/config';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // class-transformer 적용
+      transform: true,
+    }),
+  );
+
+  app.use(
+    ['/docs', '/docs-json'],
+    basicAuth({
+      challenge: true,
+      users: {
+        [configService.get('swagger.user')]:
+          configService.get('swagger.password'),
+      },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Cookie Hackathon data-server API')
+    .setDescription('API description')
+    .setVersion('0.0.1')
+    .build();
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  };
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, customOptions);
+
+  const port = configService.get('PORT') || 8081;
+  await app.listen(port);
+  console.log(`data server is running on port ${port}`);
 }
 bootstrap();
