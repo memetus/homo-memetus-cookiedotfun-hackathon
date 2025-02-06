886c810775b1ffe8bee3b17ab34c3d2f898ee634
resister-boy <hassanpumped17@gmail.com>
2025-02-06 16:10:02 +0900

    const: add base metadata set

---
+++ frontend/src/shared/constants/metadata.ts
@@ -0,0 +1,18 @@
+export const BASE_METADATA = {
+  title: 'Homo Memetus',
+  siteName: 'Homo Memetus',
+  description: 'Homo Memetus is born to abstract trading through AI evolution.',
+  keywords: [
+    'Blockchain',
+    'MemeCoin',
+    'Web3',
+    'Crypto',
+    'Solana',
+    'Trading',
+    'AI',
+    'AI Agent',
+  ],
+  url: 'https://www.homo-memetus.xyz',
+  image: 'https://i.ibb.co/18hW8Cc/homo-memetus.png',
+  ogImage: 'https://i.ibb.co/18hW8Cc/homo-memetus.png',
+};
